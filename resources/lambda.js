747e41409a9dd22fbb0fa22dbc1b54f940c63134
rqawasme <rqawasme@sfu.ca>
2022-11-07 18:31:13 -0800

    test after adding secrets

--- resources/lambda.js
+++ resources/lambda.js
@@ -1,13 +1,13 @@
-const aws = require('aws-sdk');
-const S3 = new aws.S3();
+// const aws = require('aws-sdk');
+// const S3 = new aws.S3();
 
-const bucketName = process.env.BUCKET;
+// const bucketName = process.env.BUCKET;
 
 exports.handler = async (event) => {
     try {
         console.log("did it work?", event);
-        const data = await S3.listObjectsV2({ Bucket: bucketName }).promise();
-        console.log(data)
+        // const data = await S3.listObjectsV2({ Bucket: bucketName }).promise();
+        // console.log(data)
         const response = {
             statusCode: 200,
             body: JSON.stringify([
