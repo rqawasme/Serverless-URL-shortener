e3f7c8889d704d656b137469fdd44bd5c1bb2c8b
rqawasme <rqawasme@sfu.ca>
2022-11-07 16:59:52 -0800

    ran cdk init

---
+++ test/serverless-url-shortener.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ServerlessUrlShortener = require('../lib/serverless-url-shortener-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-url-shortener-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ServerlessUrlShortener.ServerlessUrlShortenerStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
